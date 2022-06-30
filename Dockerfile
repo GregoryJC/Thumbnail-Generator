c84ebd771ba8c1dd24383f5c167943152853863c
zhonghai5 <zhonghai5@staff.sina.com.cn>
2022-06-30 19:29:49 +0800

    Completed dockerfile; docker container passed tests.

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,6 @@
 FROM python:3.8
 
-ADD logs/  /
-ADD examples/  /
-ADD finished/  /
-ADD config.py /
-ADD server.py /
-COPY requirements.txt /
-COPY run.sh /
-
+WORKDIR /thumbnail_generator
+ADD . /thumbnail_generator
 CMD /bin/sh -x run.sh
-
 EXPOSE 630
--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-cd /
+cd /thumbnail_generator
 pip3 install --upgrade pip
-pip3 install -r requirements.txt --no-cache-dir
-mkdir logs
-chmod 777 logs
-touch /logs/run.log
-touch /logs/server_gunicorn.log
-touch /logs/thumbnail_generator.log
-# /usr/local/python3/bin/gunicorn server:app -k gevent --access-logfile=logs/thumbnail_generator.log --error-logfile=logs/server_gunicorn.log --timeout 120  -b :630 -w 3
-nohup python3 server.py &
+pip3 install -r requirements.txt
+mkdir -p ./logs
+chmod 777 ./logs
+touch ./logs/run.log
+touch ./logs/server_gunicorn.log
+touch ./logs/thumbnail_generator.log
+/usr/local/bin/gunicorn server:app -k gevent --access-logfile=logs/thumbnail_generator.log --error-logfile=logs/server_gunicorn.log --timeout 120  -b :630 -w 3
