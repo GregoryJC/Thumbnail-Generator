e5118ad40faf296ef3516d8961da71aa8f3966d4
zhonghai5 <zhonghai5@staff.sina.com.cn>
2022-06-30 16:26:48 +0800

    run.sh: add logs in /logs folder

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ cd /
 pip3 install --upgrade pip
 pip3 install -r requirements.txt --no-cache-dir
 mkdir logs
-mv thumbnail_generator.log /logs/
-touch /logs/run.log
 chmod 777 logs
+touch /logs/run.log
+touch /logs/server_gunicorn.log
+touch /logs/thumbnail_generator.log
 /usr/local/python3/bin/gunicorn server:app -k gevent --access-logfile=logs/thumbnail_generator.log --error-logfile=logs/server_gunicorn.log --timeout 120  -b :630 -w 3
